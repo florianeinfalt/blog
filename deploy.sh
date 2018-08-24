1299ffdcd5ab75e45256b5e76c66f9b25cbb2d53
Florian Einfalt <florian.einfalt@me.com>
2018-08-24 19:47:24 +0100

    Move lftp install to Travis file

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
-#!/bin/sh
+#!/bin/env/bash
 set -e
-
-sudo apt-get install -y lftp
 
 # deployment via ftp upload. Using FTPS for that
 lftp -c "set ftps:initial-prot ''; set ftp:ssl-force true; set ftp:ssl-protect-data true; open ftp://$FTP_USER:$FTP_PASS@$FTP_HOST:21/htdocs; mirror -eRv public .; quit;"
